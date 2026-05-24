f4be1a00710b011dc2fdfb449315443f5bcdb0c6
j-sanin <janinsanin07@gmail.com>
2026-05-25 01:11:09 +0800

    update email to brevo smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  family: 4,
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
